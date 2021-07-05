7a1a591ddc60fef6477cd61d225ae4b4302069f8
Gajjar Mitul <gajjarmitul007@gmail.com>
2021-07-05 14:47:53 +0530

    20210705 -1447

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -10,6 +10,8 @@
 @section('styles')
     <link href="{{ asset('assets/vendors/select2/dist/css/select2.min.css') }}" rel="stylesheet" />
     <link href="{{ asset('assets/css/main.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/vendors/bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/vendors/bootstrap-timepicker/css/bootstrap-timepicker.min.css') }}" rel="stylesheet" />
 
     <style> 
         .select2-container--default .select2-selection--single{
